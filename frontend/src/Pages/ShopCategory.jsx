6d226c417e50c4ab4b47e39643af3580270a8807
Srbukmartoyan <srbukmartoyan@gmail.com>
2024-03-20 23:04:05 +0400

    added shop categories

--- frontend/src/Pages/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
+import './CSS/ShopCategory.css'
+import { ShopContext } from "../Context/ShopContext";
+import Item from "../Components/Item/Item";
 
-const ShopCategory = () => {
+const ShopCategory = (props) => {
+    const { all_product } = useContext(ShopContext);
+
     return (
-        <div>
-            
+        <div className="shop-category">
+            <img src={props.banner} alt="" />
+            <div className="shopCategory-indexSort">
+                <p>
+                    <span>showing 1 - 12 out of 54 Products</span>
+                </p>
+                <select name="sort" id="sort">
+                    <option value="kids">kids</option>
+                    <option value="men">men</option>
+                    <option value="women">women</option>
+                </select>
+            </div>
+            <div className="shopCategory-products">
+                {all_product.map((item, i) => {
+                    if (item.category === props.category) {
+                        return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+                    }
+                })}
+            </div>
         </div>
     )
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ShopContextProvider from './Context/ShopContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+root.render( // all pages can get value of ShopContext
+  <ShopContextProvider>   
+    <App /> 
+  </ShopContextProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
